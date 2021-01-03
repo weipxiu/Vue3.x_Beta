46b7d4b77927fcf2d2410fb3edaaddc5b6f33c09
李俊MacBook <lijun@021.com>
2021-01-03 20:27:47 +0800

    更新demo

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,16 @@
 <template>
   <div class="home">
-    {{sum}}
+    <p>初始值： {{sum}}
+    </p>
+    <p> 计算数字：{{sum_calc}}
+    </p>
+    <p @click="changSum">点击改变{{sum}}</p>
   </div>
 </template>
 
 <script>
 // import HelloWorld from '@/components/HelloWorld.vue'
-import { reactive } from 'vue';
+import { reactive, toRefs, computed } from 'vue';
 export default {
   name: 'Home',
   components: {
@@ -14,10 +18,20 @@ export default {
   },
   setup() {
     const state = reactive({
-      sum: 1
+      sum: 1,
+      sum_calc: computed(() => {
+        return state.sum + 5
+      })
     })
+
+    const methods = {
+      changSum: () => {
+        state.sum += 1
+      }
+    }
     return {
-      ...state
+      ...toRefs(state),
+      ...methods
     }
   }
 }
